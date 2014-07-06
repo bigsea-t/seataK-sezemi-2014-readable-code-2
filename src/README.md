37e38460088a0ab08fdf190775275d9444007579
taikai <297.1951@gmail.com>
2014-07-06 11:53:56 +0900

    spec:6 complete

--- src/README.md
+++ src/README.md
@@ -5,7 +5,9 @@
 ```
 $ ruby task.rb
 please type file name to import
-> recipe-data.txt
-オムライス
+>recipe-data.txt ##読み込むファイル名を指定
+please type id to output ##出力するidを指定
+>3
+3:杏仁豆腐
 ```
 
--- src/task.rb
+++ src/task.rb
@@ -1,19 +1,37 @@
 
 # getting file name to import
-
 print "please type file name to import \n>"
 
 #use chomp to delete newline characher
 file_name = gets().chomp
 
+# select id
+print "please type id to output \n>"
+
+#use chomp to delete newline characher
+selected_id = gets().chomp.to_i
+
 # open file in reading mode
 file  = open(file_name)
 
-
-#output
+recipe_list = []
+id_counter = 0
 while line = file.gets
-  puts line
+  id_counter += 1
+  recipe = {
+    id: id_counter,
+    val: line
+  }
+  recipe_list.push(recipe)
 end
 
 #DO NOT forget closing file
 file.close
+
+#output
+recipe_list.each do |recipe|
+  if recipe[:id] == selected_id then
+    puts recipe[:id].to_s + ":" + recipe[:val]
+  end
+end
+
